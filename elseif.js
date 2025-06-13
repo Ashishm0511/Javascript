e39f5e5ccd3b573ad1b1777113a0b5f493b90bcf
Ashish <ashishm1900@gmail.com>
2025-06-13 23:56:05 +0530

    added file

---
+++ elseif.js
@@ -0,0 +1,24 @@
+// let tmp = 0;
+// if(tmp < 0){
+//     console.log("Extremly cold outside");
+// }else if(tmp < 16){
+//     console.log("Its cold outside");
+// }else if(tmp < 25){
+//     console.log("Weather is Okay");
+// }else if(tmp < 35){
+//     console.log("lets go for swim");
+// }else if(tmp < 45){
+//     console.log("Turn on Ac");
+// }else {
+//     console.log("Too hot");
+// }
+let newtmp = 26;
+if(newtmp>40){
+    console.log("it is very hot");
+}else if(newtmp > 25){
+    console.log("Looks good weather");
+}else if(newtmp > 10){
+    console.log("cold weather");
+}else {
+    console.log("too cold")
+}
