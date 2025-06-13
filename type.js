f23ce58b9cb80acb0dc4ffcd571a50315661463f
Ashish <ashishm1900@gmail.com>
2025-06-14 00:29:13 +0530

    checked type and changed from string to number

--- type.js
+++ type.js
@@ -5,3 +5,8 @@ console.log(typeof age); // number
 // to change from numbaer to string  
 age = String(age);
 console.log(typeof age);
+
+// from number to string
+my_name = Number(my_name);
+console.log(typeof my_name);
+
