1ea89bbd8065acbac46f0925756d9e200d93c9fb
Ashish <ashishm1900@gmail.com>
2025-06-13 23:42:09 +0530

    Added file

---
+++ for.js
@@ -0,0 +1,11 @@
+// let i = 0;
+// for(;i<10; i++){
+//     console.log(i);
+// }
+// console.log(i);
+let total =0;
+let num= 10;
+for(i = 1; i<=num; i++){
+    total = total+i;
+}
+console.log(total);
