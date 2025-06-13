f7b8d222f8026ca6c7c25f8c3d712be97fe66a39
Ashish <ashishm1900@gmail.com>
2025-06-13 23:55:47 +0530

    added file

---
+++ arr.js
@@ -0,0 +1,16 @@
+// let fruits = ["Grapes", "Mango", "Apple"];
+// let numbers = [1,2,3,4,5,6];
+// let news = [1,,2.3,4, "Ashish", "sam"];
+// let file = 23;
+// let files = "true";
+// console.log(fruits);
+// console.log(numbers);
+// console.log(news);
+// console.log(typeof file);
+// console.log(typeof files);
+
+let string = "17";
+let string1 = "18";
+let newString = +string + +string1;
+console.log(newString);
+console.log(typeof newString);
