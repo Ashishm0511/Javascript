68921bfdb98009d15c846656b1c71ba5d984410c
Ashish <ashishm1900@gmail.com>
2025-06-18 23:58:30 +0530

    added ifelse

---
+++ ifelse.js
@@ -0,0 +1,19 @@
+let value = 1;
+if(value > 10){
+    console.log("This no is greater");
+}
+else {
+    console.log("No is less");
+}
+// console.log("Ashsi");
+let num = 14;
+if(num%2 ===0){
+    console.log(num ,"no is even");
+}
+else {
+    console.log("no is odd");
+}
+// ternary operator / conditional 
+let age = 3;
+let drink = age >= 5 ? "coffee" : "milk";
+console.log(drink);
