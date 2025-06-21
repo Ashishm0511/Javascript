b0f3f87c6b4b807bc71cb67bbee3ee00bc6336bd
Ashish <ashishm1900@gmail.com>
2025-06-21 21:39:12 +0530

    Added

---
+++ let.js
@@ -0,0 +1,8 @@
+// String indexing
+
+let firstName = "Ashish  Maurya"
+
+console.log(firstName);
+console.log(firstName[3]);
+console.log(firstName.length);
+console.log(firstName[firstName.length-4]);
