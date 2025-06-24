487dd0a452d09d8623037722ce4df52992107346
Ashish <ashishm1900@gmail.com>
2025-06-24 22:08:01 +0530

    Added

---
+++ iterable.js
@@ -0,0 +1,12 @@
+// iterables
+// jispe hum for of loop lga sake
+// string and Array are iterabls
+
+// const firstName = "Ashish";
+// for(let char of firstName){
+//     console.log(char);
+// }
+
+const firstName = "Ashish";
+console.log(firstName.length);
+console.log(firstName[2]);
