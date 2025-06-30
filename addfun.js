aa0a4881b46445aaf0394b62779d1bb89d80ffc5
Ashish <ashishm1900@gmail.com>
2025-06-30 16:11:21 +0530

    used function

--- addfun.js
+++ addfun.js
@@ -6,3 +6,7 @@ const calculator ={
         return x - y;
     }
 }
+const sum =calculator.add(13,5);
+const diff = calculator.subtract(12,3);
+console.log("Sum:", sum);
+console.log("Difference:", diff);
